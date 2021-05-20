5c01457f3fd183970c06ad8ed235b8f8b5cab026
Daniela Sannino <daniela.sannino1@gmail.com>
2021-05-19 20:55:12 -0400

    add Title

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ value }) => {
+  const array = Object.values(value);
   return (
     <div className="tile-container">
-      
+      {array.map((data, index) => {
+        if (index === 0) {
+          return <p className='title-title' key={index}>{data}</p>
+        }
+        return <p className='title' key={index}>{data}</p>
+      })
+      }
     </div>
   );
 };
